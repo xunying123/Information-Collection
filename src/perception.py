75a6c538b0f83f4fcdcc5f65a41939172d109ef4
xunying123 <119675097+xunying123@users.noreply.github.com>
2024-09-19 16:12:26 +0800

    Update perception.py

--- src/perception.py
+++ src/perception.py
@@ -1,19 +1,15 @@
 from datetime import datetime
-from utils import extract_domain, headers, normalize_url, is_ad, read_content, save_content
+from utils import extract_domain, headers, normalize_url, read_content, save_content, title_, summary_, is_ad, check_
 import asyncio
 from urllib.parse import urljoin
 from playwright.async_api import async_playwright
 from crawler import crawl
-from GeneralAgent import Agent
+import time
 
 proxies = {
     'http': 'http://127.0.0.1:7890',  # 为HTTP设置代理，端口根据实际情况修改
     'https': 'https://127.0.0.1:7890',  # 为HTTPS设置代理，端口根据实际情况修改
 }
-
-Summary = Agent('下面会给你一篇中文文章，我需要你对它进行总结，是的最终我得到的总结长度在300-400之间', hide_python_code=True)
-
-Translate = Agent('下面会给你一个英文标题，请把它翻译成中文', hide_python_code=True)
 
 async def fetch_website_content(url):
     async with async_playwright() as p:
@@ -51,52 +47,54 @@ async def fetch_website_content(url):
         return extract_links
 
 def preception(url):
-    print(f"Fetching {url}")    
+    print(f"Fetching {url}", flush=True)    
     current_links = asyncio.run(fetch_website_content(url))
-    # print(f"Total {len(current_links)} articles found.")  
     current_date = datetime.now()
+
     folder_name = current_date.strftime("%Y-%m-%d")
-    folder_path = f"data/articles/{folder_name}/"     
-    filename = "data/saved_links/" + extract_domain(url) + '.json'
+    folder_path = f"/home/dic/Information-Collection/src/data/articles/{folder_name}/"     
+    filename = "/home/dic/Information-Collection/src/data/saved_links/" + extract_domain(url) + '.json'
+
     article_path = folder_path + extract_domain(url) + '.json'
     previous_links = read_content(filename)    
     new_links = list(set(current_links) - set(previous_links))
     current_links = list(set(current_links) | set(previous_links))  
     save_content(current_links, filename)  
+
     if new_links:
-        print(f"New \033[92m{len(new_links)}\033[0m articles found.")
+        print(f"New {len(new_links)} articles found.", flush=True)
         articles = []
         for link in new_links:
-            title, content = crawl(link)
+            if check_(link):
+                continue
+            title, content = crawl(link, url)
             if not title or not content:
                 continue
-            # summary = Summary.run(content)
+            summary = summary_(content)
+            title = title_(title)
             publish_time = current_date.strftime("%Y-%m-%d %H:%M")
             data = {
                 "title": title,
-                "content": content,
+                "content": summary,
+                "full_content":content,
                 "source_url": link,
                 "publish_time": publish_time
             }
             articles.append(data)
-            # print(f"\033[92m{title}\033[0m saved.")
+            time.sleep(0.1)
         save_content(articles, article_path)
     else:
-        print(f"\033[31mNo\033[0m new articles found.")
+        print(f"No new articles found.", flush=True)
 
 def add_website(url):
-    print(f"Fetching {url}")    
+    print(f"Adding {url}", flush=True)    
     current_links = asyncio.run(fetch_website_content(url)) 
-    filename = "data/saved_links/" + extract_domain(url) + '.json' 
+    filename = "/home/dic/Information-Collection/src/data/saved_links/" + extract_domain(url) + '.json' 
     save_content(current_links, filename)  
 
 def main():
-    for i in range(0, 100):
-        url = 'https://edu.cri.cn/'
-        preception(url)
-    
-    # for url in websites:
-    #     preception(url)
+    url = 'https://edu.cri.cn/'
+    preception(url)
 
 if __name__ == '__main__':
     main()
