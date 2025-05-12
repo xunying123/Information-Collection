db8c7fc5a8d154da6226e5b087be8d56e17ba7ee
xunying123 <xun_ying123@163.com>
2025-05-12 10:42:57 +0800

    (feat)backend:more and more bugs

--- src/perception.py
+++ src/perception.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from src.utils import extract_domain, headers, normalize_url, read_content, save_content, title_, summary_, is_ad, check_, what_score, what_word, get_keywords_id, logging
+from src.utils import extract_domain, headers, normalize_url, read_content, save_content, title_, summary_, is_ad, check_, what_score, what_word, get_keywords_id, logging, wash_url
 import asyncio
 from urllib.parse import urljoin
 from playwright.async_api import async_playwright
@@ -8,13 +8,41 @@ import time
 import random
 from src.data import push_page_to_db, push_keyword_to_db
 import os
+import requests
+from bs4 import BeautifulSoup
 
 proxies = {
     'http': 'http://127.0.0.1:7890',  # 为HTTP设置代理，端口根据实际情况修改
     'https': 'https://127.0.0.1:7890',  # 为HTTPS设置代理，端口根据实际情况修改
 }
 
+def get_article_links(url):
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Error fetching {url}: {e}")
+        return []
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = set()
+
+    for item in soup.find_all('div', class_='item'):
+        aid_tag = item.find('textarea', class_='item-aid')
+        type_tag = item.find('textarea', class_='item-addltype')
+        host_tag = item.find('textarea', class_='item-cnf-host')
+        if aid_tag and type_tag and host_tag:
+            aid = aid_tag.text.strip()
+            addltype = type_tag.text.strip()
+            host = host_tag.text.strip()
+            url = f"https://{host}/{addltype}/{aid}"
+            links.add(url)
+
+    return list(set(links))
+
 async def fetch_website_content(url):
+    if "huanqiu" in "https://world.huanqiu.com/":
+        return get_article_links(url)
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True)
         page = await browser.new_page()
@@ -24,9 +52,11 @@ async def fetch_website_content(url):
             await page.goto(base_url, wait_until='domcontentloaded', timeout=60000)     
 
         # await page.goto(base_url, timeout=60000)
+            max_scroll_times = 30  # 最多滚动 30 次
+            scroll_count = 0
             last_height = await page.evaluate("document.body.scrollHeight")
 
-            while True:            
+            while scroll_count < max_scroll_times:            
                 await page.evaluate("window.scrollTo(0, document.body.scrollHeight);")                        
                 await page.wait_for_timeout(2000)  # 等待2秒            
                 new_height = await page.evaluate("document.body.scrollHeight")
@@ -34,14 +64,17 @@ async def fetch_website_content(url):
                     break  # 如果页面高度没有变化，则停止滚动
 
                 last_height = new_height
+                scroll_count += 1
 
         except:
             await page.goto(base_url, wait_until='networkidle', timeout=60000)     
 
         # await page.goto(base_url, timeout=60000)
             last_height = await page.evaluate("document.body.scrollHeight")
+            max_scroll_times = 30 # 最多滚动 30 次
+            scroll_count = 0
 
-            while True:            
+            while scroll_count < max_scroll_times:            
                 await page.evaluate("window.scrollTo(0, document.body.scrollHeight);")                        
                 await page.wait_for_timeout(2000)  # 等待2秒            
                 new_height = await page.evaluate("document.body.scrollHeight")
@@ -49,6 +82,7 @@ async def fetch_website_content(url):
                     break  # 如果页面高度没有变化，则停止滚动
 
                 last_height = new_height
+                scroll_count += 1
         
         links = await page.query_selector_all("a")
 
@@ -98,13 +132,14 @@ def preception(web):
                     title = title_(title)
                     publish_time = current_date.strftime("%Y-%m-%d %H:%M")
                     score = what_score(content)
+                    source_url = wash_url(link)
                     if times:
                         publish_time = times
                     data = {
                         "title": title,
                         "content": summary,
                         "full_content":content,
-                        "source_url": link,
+                        "source_url": source_url,
                         "publish_time": publish_time,
                         'site_id': web['id'],
                         'score': score,
