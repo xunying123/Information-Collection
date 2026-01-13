21a503400560053702cfc0748747446c83a98686
Wankupi <kunpengwang@sjtu.edu.cn>
2026-01-13 21:12:19 +0800

    feat(web): extent time detail to minutes

--- web/src/utils/timeUtils.ts
+++ web/src/utils/timeUtils.ts
@@ -3,11 +3,8 @@ export const showTime = (date: string) => {
   const dateObj = new Date(date)
   const diff = now.getTime() - dateObj.getTime()
   const diffHours = diff / 1000 / 60 / 60
-  if (diffHours < 24) {
-    return `${Math.floor(diffHours)} 小时前`
-  } else if (diffHours < 48) {
-    return '1 天前'
-  } else {
-    return dateObj.toLocaleDateString()
-  }
+  if (diffHours < 1) return `${Math.floor(diffHours * 60)} 分钟前`
+  else if (diffHours < 24) return `${Math.floor(diffHours)} 小时前`
+  else if (diffHours < 48) return '1 天前'
+  else return dateObj.toLocaleDateString()
 }
